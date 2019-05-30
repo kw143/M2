8a224c3f5026eae486c785c4bb58dab0376cbd54
W Touchstone <wtouchstone3@gmail.com>
2019-05-30 14:28:43 -0400

    added method for person3 and added name to antlabmain

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,16 +65,16 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("rpatel406");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("Dillon Greer");
-                  toPrint += p4.toString("dgreer33");
-                  //Person5 replace P4 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Jiaming Wang");
-                  toPrint += p5.toString("jwang952");
+		  Person3 p3 = new Person3("William Touchstone");
+		  toPrint += p3.toString("wtouchstone3");
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+          Person4 p4 = new Person4("Dillon Greer");
+          toPrint += p4.toString("dgreer33");
+          //Person5 replace P4 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("Jiaming Wang");
+          toPrint += p5.toString("jwang952");
 
         System.out.println(toPrint);
 
